cfe2528c760bff03a62fd59c8452760e45dc8f8b
Michal Czyszyn <michalczyszyn.edu@gmail.com>
2026-09-02 08:05:34 -0300

    agenda page já busca os agendamentos no banco

--- src/pages/AgendaPage.tsx
+++ src/pages/AgendaPage.tsx
@@ -1,5 +1,7 @@
 import { useAuth } from "../contexts/AuthContext";
-import { useState } from "react";
+import { use, useEffect, useState } from "react";
+import type { agendamento } from "../types/agendamento";
+import { supabase } from "../service/supabase";
 
 export function AgendaPage() {
   //estados de logout
@@ -7,16 +9,15 @@ export function AgendaPage() {
   const [error, setError] = useState("");
 
   //estados de busca de pacientes 
-  const [appointments, setAppointments] = useState("");
-  const [error, setError] = useState(""); 
-  const [isloading, setLoading ] = useState(false);
+  const [appointments, setAppointments] = useState<agendamento[]>([])
+  const [errorData, setErrorData] = useState(""); 
+  const [isloading, setLoading ] = useState(true);
 
 
-  
+
 
   const  handleLogOut = async () => {
       try {
-
         await signOut(); 
         
       }catch (errorLogout) {
@@ -28,14 +29,68 @@ export function AgendaPage() {
       }
   } 
 
+  useEffect(() => {
+    //função de busca 
+    const fetchAppointments = async () => {
+      try{
+        const { data, error: supabaseError } = await supabase.from('agendamento').select('*, paciente:paciente_fk ( nome ), dentista:dentista_fk ( nome )');
+   
+        if(supabaseError){
+          setErrorData(supabaseError.message);
+        }
+
+        if(data){
+          setAppointments(data);
+          return;
+        }
+
+      }catch(error){
+            if(error instanceof Error){
+            setErrorData(error.message);
+            return
+            }
+
+            else{
+             setErrorData("Erro inesperado de conexão");
+            }
+      } finally{
+          setLoading(false);        
+      } 
+    }
+
+    //chamando a função 
+    fetchAppointments()
+  },[]);
 
   return(
     <div>
-      <h1>Bem-vindo à Agenda do Consultório! (Rota Protegida)</h1>
+      {isloading && <p>Carregando agendamentos...</p>}
       {error && <p>{error}</p>}
+      {errorData && <p>{errorData}</p>}
+
       <button type="button" onClick={handleLogOut}>log out</button>
-    </div> 
+      <ul>
+        {appointments.map((item) => (
+          <li key={item.id}>
+            <div>
+              <p>Paciente: {item.paciente.nome ?? "Não informado"}</p>
+              <p>Procedimento: {item.procedimento} ({item.status})</p> 
+              <p>Dentista: {item.dentista?.nome ?? "Não informado"}</p> 
+              <p>
+                  Data: {new Date(item.data_horario).toLocaleString('pt-BR', {
+                    day: '2-digit',
+                    month: '2-digit',
+                    year: 'numeric',
+                    hour: '2-digit',
+                    minute: '2-digit',
+                  })}
+              </p>
+            </div><br/>
+          </li>
+        ))}
+      </ul>
+    </div>
+    
     
   )
-
 }
--- src/types/agendamento.ts
+++ src/types/agendamento.ts
@@ -1,10 +1,13 @@
 export type Status = 'confirmado' | 'agendado' | 'cancelado' | 'realizado' | 'falta';
 
 export type agendamento = {
+    id:string;
     dentista_fk: string;
     paciente_fk: string;
     procedimento: string;
     data_hora_fim: string;
     status: Status;
     data_horario:string;
+    paciente?: { nome: string } | null;
+    dentista?: { nome: string } | null;
 }
